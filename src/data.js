095706f28ee917515992d4e3ee8d914ec7506007
Divv Saxena <saxenadivv@gmail.com>
2023-09-28 23:36:53 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -21,8 +21,8 @@ export default [
         title: "Mount Fuji",
         location: "JAPAN",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "I'll visit it soon",
+        endDate: "",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
@@ -30,8 +30,8 @@ export default [
         title: "Sydney Opera House",
         location: "AUSTRALIA",
         googleMapsUrl: "https://maps.app.goo.gl/92doHxajWHFmrdeN7",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
+        startDate: "I'll visit it soon",
+        endDate: "",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imageUrl:"https://lh3.googleusercontent.com/p/AF1QipMGKLcJUmd_osXT86GLgGdZiv0RnFbs3OhDWqe4=s1360-w1360-h1020"
     },
@@ -39,8 +39,8 @@ export default [
         title: "Geirangerfjord",
         location: "NORWAY",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "01 Oct, 2021",
-        endDate: "18 Nov, 2021",
+        startDate: "I'll visit it soon",
+        endDate: "",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl:"https://media1.thrillophilia.com/filestore/thvc5gja9583qe4jn1cq15soso6u_1599212950_Geiranger_Fjord.jpg?w=auto&h=600"
     }
